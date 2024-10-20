cb6ff103a9d82f7e37a2032ffe02cadcab38a261
Carlos Palacios <carlospalaciosmena@gmail.com>
2024-10-20 04:07:46 -0500

    adding ms_reserve to the project

---
+++ ms_reserve/src/main/java/com/ms_reserve/reserve/ReserveApplication.java
@@ -0,0 +1,17 @@
+package com.ms_reserve.reserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class ReserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReserveApplication.class, args);
+	}
+		// ms clients communication
+		// database persistence
+		// validate the requests and responses, add exception handling
+}
